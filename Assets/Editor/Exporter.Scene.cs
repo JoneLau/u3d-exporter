24fb5f34a8636e732af5d17c57d2dd7b36aeb885
Johnny Wu <KarasAya@gmail.com>
2017-09-06 12:19:22 +0800

    fix skinned model transform error

--- Assets/Editor/Exporter.Scene.cs
+++ Assets/Editor/Exporter.Scene.cs
@@ -41,23 +41,42 @@ namespace exsdk {
       // name
       result.name = _go.name;
 
-      // translation
-      // NOTE: convert LH to RH
-      result.translation[0] = _go.transform.localPosition.x;
-      result.translation[1] = _go.transform.localPosition.y;
-      result.translation[2] = -_go.transform.localPosition.z;
+      // NOTE: skinned mesh node will use identity matrix
+      if (_go.GetComponent<SkinnedMeshRenderer>() != null) {
+        // translation
+        result.translation[0] = 0.0f;
+        result.translation[1] = 0.0f;
+        result.translation[2] = 0.0f;
 
-      // rotation
-      // NOTE: convert LH to RH
-      result.rotation[0] = -_go.transform.localRotation.x;
-      result.rotation[1] = -_go.transform.localRotation.y;
-      result.rotation[2] = _go.transform.localRotation.z;
-      result.rotation[3] = _go.transform.localRotation.w;
+        // rotation
+        result.rotation[0] = 0.0f;
+        result.rotation[1] = 0.0f;
+        result.rotation[2] = 0.0f;
+        result.rotation[3] = 1.0f;
 
-      // scale
-      result.scale[0] = _go.transform.localScale.x;
-      result.scale[1] = _go.transform.localScale.y;
-      result.scale[2] = _go.transform.localScale.z;
+        // scale
+        result.scale[0] = 1.0f;
+        result.scale[1] = 1.0f;
+        result.scale[2] = 1.0f;
+      } else {
+        // translation
+        // NOTE: convert LH to RH
+        result.translation[0] = _go.transform.localPosition.x;
+        result.translation[1] = _go.transform.localPosition.y;
+        result.translation[2] = -_go.transform.localPosition.z;
+
+        // rotation
+        // NOTE: convert LH to RH
+        result.rotation[0] = -_go.transform.localRotation.x;
+        result.rotation[1] = -_go.transform.localRotation.y;
+        result.rotation[2] = _go.transform.localRotation.z;
+        result.rotation[3] = _go.transform.localRotation.w;
+
+        // scale
+        result.scale[0] = _go.transform.localScale.x;
+        result.scale[1] = _go.transform.localScale.y;
+        result.scale[2] = _go.transform.localScale.z;
+      }
 
       // children
       result.children = new List<int>();
